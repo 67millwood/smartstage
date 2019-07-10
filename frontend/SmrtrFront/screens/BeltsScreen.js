bd03e55795b275bcc71a0c3f9a7e27a35d38494e
Keith McSpurren <info@track2development.com>
2019-07-10 07:53:26 -0400

    belt data rendered onto screen

--- frontend/SmrtrFront/screens/BeltsScreen.js
+++ frontend/SmrtrFront/screens/BeltsScreen.js
@@ -1,13 +1,69 @@
 import React, { Component, Fragment } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity, AsyncStorage, FlatList } from 'react-native';
 import ProfileIcon from '../navigation/ProfileIcon';
 
 
 
 export default class BeltsScreen extends Component {
+  constructor(){
+    super();
+    this.state = {
+      datasource: []
+    }
+  }
+
+
+  getallbelts = async () => {
+    const userToken = await AsyncStorage.getItem('LoginToken');
+    console.log(userToken)
+
+    try {
+        return fetch('http://localhost:8080/api/userbelts/', {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${userToken}`,
+          },
+      })
+      .then(response => {
+        if(!response.ok) {
+          console.log('crap')      
+          } else {
+            response.json().then(data => {
+              console.log(data)
+              this.setState({
+                datasource: data
+              }
+              )
+            })
+        }
+      })}
+    catch(error) {
+      console.log('no belts coming');
+    };
+  }
+
+  renderlist = ({ item }) => (
+    <Text>User: {item.user}, Belt Level: {item.belt_level}</Text>
+  );
+
+
   render() {
     return (
-      null
+      <View>
+        <TouchableOpacity
+        style={{backgroundColor: 'aqua' }} 
+        onPress={this.getallbelts}
+          >
+            <Text>Give me Belts</Text>
+        </TouchableOpacity>
+        <FlatList
+        data={this.state.datasource}
+        renderItem={this.renderlist}
+        keyExtractor={(item) => String(item.id)}
+        />
+      </View>
     );
   }
 }
