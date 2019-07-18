e1298a49ad2f69611549a2e164cdf4747610b3db
Keith McSpurren <info@track2development.com>
2019-07-18 14:10:10 -0400

    Questionserializer now updated with proper level of questions

--- backend/content/api.py
+++ backend/content/api.py
@@ -11,7 +11,11 @@ class QuestionViewSet(generics.ListAPIView):
     ]
     serializer_class = QuestionSerializer
     def get_queryset(self):
-        questionlist = Question.objects.one_level_questions(category=1, level=1)
+        user = self.request.user
+        beltlist = UserBelts.objects.all_belts(user=user)
+        highest_belt = beltlist['highest_belt_level']
+
+        questionlist = Question.objects.one_level_questions(category=1, level=highest_belt)
         return questionlist['result']
 
 # Reading Viewset
@@ -20,7 +24,6 @@ class ReadingViewSet(generics.ListAPIView):
         permissions.IsAuthenticated,
     ]
     serializer_class = ReadingSerializer
-    # queryset = Reading.objects.filter(belt_level=1, category=1)
     def get_queryset(self):
         user = self.request.user
         beltlist = UserBelts.objects.all_belts(user=user)
