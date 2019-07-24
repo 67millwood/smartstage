f81ac32c4c1d31ca38fd7d2ef208c887aa250126
Keith McSpurren <info@track2development.com>
2019-07-24 16:09:24 -0400

    modified to pull 4 random questions of any type

--- backend/content/manager.py
+++ backend/content/manager.py
@@ -12,17 +12,4 @@ class QuestionManager(models.Manager):
 
     return question_info
 
-  def better_one_level_questions(self, category, level):
-    result = super().get_queryset().filter(category=category, belt_level=level)
-    question_ids = result.values('id', 'qtype')
-    '''
-    answered = UserAnswer.objects.all_attempts(user=user)
-    custom_answer_query = answered['correct_answers']
-    '''
-    question_list = []
-    for item in list(question_ids):
-      question_list.append(item)
-
-    return question_list
-
 
--- backend/data/scenarios/question_creation.py
+++ backend/data/scenarios/question_creation.py
@@ -1,4 +1,4 @@
-from content.models import Question
+from content.models import Question, MultipleChoice, TrueFalse, Rating, Ranking
 from belts.models import UserAnswer, UserBelts
 #from content.views import starting_point
 import random
@@ -8,9 +8,7 @@ def question_set(user, category):
     belts = UserBelts.objects.all_belts(user=user)
 
     user_questions = Question.objects.one_level_questions(category, belts['highest_belt_level'])
-    
-    print(Question.objects.better_one_level_questions(category, belts['highest_belt_level']))
-    
+        
     custom_question_query = user_questions['result']
     # previously answered questions that were correct=True
     answered = UserAnswer.objects.all_attempts(user=user)
@@ -18,15 +16,43 @@ def question_set(user, category):
 
     # create a subset of all available questions for that belt level MINUS all correctly answered questions
     question_ids = custom_question_query.values('id').difference(custom_answer_query.values('question_id'))
+    #print(question_ids)
     question_id_list = []
     for question in question_ids:
       question_id_list.append(question['id'])
     shuffled_ids = random.sample(question_id_list, len(question_id_list))
-
+    #print(shuffled_ids)
+    
+    web_questions = []
     question_session = []
     for item in shuffled_ids:
-      question_session.append(Question.objects.get(pk=item))
+      try:
+        nice = MultipleChoice.objects.get(pk=item)
+        question_session.append(nice)
+      except:
+        print('not')
+      try:
+        nice = TrueFalse.objects.get(pk=item)
+        question_session.append(nice)
+      except:
+        print('not')
+      try:
+        nice = Rating.objects.get(pk=item)
+        question_session.append(nice)
+      except:
+        print('not')
+      try:
+        nice = Ranking.objects.get(pk=item)
+        question_session.append(nice)
+      except:
+        print('not')
+ 
+      web_questions.append(Question.objects.get(pk=item))
+    print(question_session)
+    #for item in shuffled_ids:
 
-    return question_session[:4]
+
+    #print(question_session[:4])
+    return web_questions[:4]
 
 
