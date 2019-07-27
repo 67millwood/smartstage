3e33d17d96d057e9a0bdae8d1d4ec2a8274e7da0
Keith McSpurren <info@track2development.com>
2019-07-26 22:46:11 -0400

    true false functioning properly

--- frontend/SmrtrFront/components/questionpieces/truefalse.js
+++ frontend/SmrtrFront/components/questionpieces/truefalse.js
@@ -11,7 +11,22 @@ import {
 import { styles } from './questionstyles';
 
 export default class TrueFalseQuestion extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            choiceTrue: false,
+            choiceFalse: false,
+        }
+    }
 
+    selectedChoice = (choice) => {
+        this.setState({ 
+            choiceTrue: false,
+            choiceFalse: false,
+        })
+        this.setState({ [`${choice}`]: true}) 
+        
+    }
 
     render() {
         const question = this.props.info
@@ -24,14 +39,20 @@ export default class TrueFalseQuestion extends Component {
                     {question.question_text}
                 </Text>
                 <TouchableOpacity
-                    style={styles.trueChoice} 
+                    style={this.state.choiceTrue ? {...styles.trueChoice, ...styles.selected} : styles.trueChoice}
+                    onPress={() => {
+                        this.selectedChoice('choiceTrue')
+                    }}
                       >
                     <Text style={styles.sections}>TRUE</Text>
 
                 </TouchableOpacity>
 
                 <TouchableOpacity
-                    style={styles.falseChoice} 
+                    style={this.state.choiceFalse ? {...styles.falseChoice, ...styles.selected} : styles.falseChoice}
+                    onPress={() => {
+                        this.selectedChoice('choiceFalse')
+                    }}
                       >
                     <Text style={styles.sections}>FALSE</Text>
 
