5ea80e7476edc2dcce7661ad8fcddc12c7a07bc2
Keith McSpurren <info@track2development.com>
2019-07-26 23:48:57 -0400

    shuffle ranking now working

--- frontend/SmrtrFront/components/questionpieces/rankingquestion.js
+++ frontend/SmrtrFront/components/questionpieces/rankingquestion.js
@@ -9,9 +9,44 @@ import {
 } from 'react-native';
 
 import { styles } from './questionstyles';
+import DraggableFlatList from 'react-native-draggable-flatlist';
 
 
 export default class RankingQuestion extends Component {
+    state = {
+
+        data: [this.props.info.choice_1, this.props.info.choice_2, this.props.info.choice_3, this.props.info.choice_4].map((d, index) => ({
+          key: `item-${index}`,
+          label: d,
+        }))
+      }
+
+
+
+    renderItem = ({ item, index, move, moveEnd, isActive }) => {
+        const schemes = ['coral', 'green', 'aqua', 'burlywood']
+        return (
+          <TouchableOpacity
+            
+            style={{ 
+              height: 100, 
+              backgroundColor: isActive ? 'blue' : `${schemes[index]}`,
+              marginLeft: 5,
+              marginRight: 5,
+              marginBottom: 5,
+              alignItems: 'center', 
+              justifyContent: 'center' 
+            }}
+            onLongPress={move}
+            onPressOut={moveEnd}
+          >
+            <Text style={{ 
+              fontSize: 17,
+            }}>{item.label}</Text>
+            <Text>{index}</Text>
+          </TouchableOpacity>
+        )
+      }
 
 
     render() {
@@ -21,9 +56,25 @@ export default class RankingQuestion extends Component {
                 <Text style={styles.title}>
                     Rank these 4 choices...
                 </Text>
+                
                 <Text style={styles.questionText}>
                     {question.question_text}
                 </Text>
+                <DraggableFlatList
+                    data={this.state.data}
+                    renderItem={this.renderItem}
+                    keyExtractor={(item, index) => `draggable-item-${item.key}`}
+                    
+                    onMoveEnd={({ data }) => this.setState({ data })}
+                    />
+
+                
+            </View>
+        )
+    }
+}
+
+/* old view
                 <TouchableOpacity style={styles.choice1}>
                     <Text>
                     {question.choice_1}
@@ -44,8 +95,4 @@ export default class RankingQuestion extends Component {
                     {question.choice_4}
                     </Text>
                 </TouchableOpacity>
-
-            </View>
-        )
-    }
-}
+*/
