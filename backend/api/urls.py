d432720064ac76d5f0243001cb7c19e514ecb651
Keith McSpurren <info@track2development.com>
2019-08-14 14:54:19 -0400

    minor deletions of unused files

--- backend/api/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import routers
-from .api import UserAnswerViewSet
-
-router = routers.DefaultRouter()
-router.register('', UserAnswerViewSet, 'useranswer')
-
-urlpatterns = [
-  router.urls
-  ]
