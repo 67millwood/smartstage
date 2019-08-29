c244403db69d94ba3abbc1a85bdb1e48ca797960
Keith McSpurren <info@track2development.com>
2019-08-29 12:31:40 -0400

    medium term consistency working, refactored to eliminate loops in consistency

--- backend/data/analysis/consistency.py
+++ backend/data/analysis/consistency.py
@@ -4,46 +4,24 @@ from belts.models import UserAnswer
 
 def short_term(user):
     # set today and the start of the range
-    seven_days_ago = date.today() - timedelta(7)
+    seven_days = date.today() - timedelta(7)
     today = date.today()
     interval = timedelta(days=1)
-    
-    # create the list of ALL dates when questions were answered
-    all_question_dates = []
-    real_questions = UserAnswer.objects.all().filter(answer_date__gt=seven_days_ago).filter(user=user)
-    for question in real_questions:
-        # need to convert datetime.datetime objects in database to .date()
-        all_question_dates.append(question.answer_date.date())
-    print(seven_days_ago)
-    print(all_question_dates)
-    # user the interval to count down the days individual between start date and today
-    # check each date (converted to .date()) when a question was answered to see if occured on a day with no questions so far
-    # add it to the collection of days and discard it from the original list
-    # repeat until add days have been checked
-    
-    question_dates = []
 
-    while seven_days_ago <= today:
-        #print(seven_days_ago)
-        for qdate in all_question_dates:
-            if qdate == seven_days_ago:
-                if qdate not in question_dates:
-                    question_dates.append(qdate)
-                all_question_dates.remove(qdate)
-        seven_days_ago += interval
+    # jesus...it's one line....https://docs.djangoproject.com/en/2.2/ref/models/querysets/#dates
+    last7days = UserAnswer.objects.dates('answer_date', 'day').filter(answer_date__gt=seven_days).filter(user=user).count()
     
-    # the number of unique dates is called hits....
-    hits = len(question_dates)
-    print('This many unique days in the p7:', hits)
-    short_term_rating: ''
+    print('This many unique days in the p7:', last7days)
     # give a rating to the consistency over the past 7 days
-    if hits <= 1:
+    short_term_rating: ''
+
+    if last7days <= 1:
         short_term_rating = 'Poor'
-    elif hits <=2:
+    elif last7days <=2:
         short_term_rating = 'Moderate'
-    elif hits <=3:
+    elif last7days <=3:
         short_term_rating = 'Good'
-    elif hits <=4:
+    elif last7days <=4:
         short_term_rating = 'Great'
     else:
         short_term_rating = 'Amazing'
@@ -52,16 +30,25 @@ def short_term(user):
 
 def medium_term(user):
     today = date.today()
-    thirty_days = today - timedelta(30)
     ten_days = today - timedelta(10)
-    twenty_days = ten_days - timedelta(10)
+    twenty_days = today - timedelta(20)
+    thirty_days = today - timedelta(30)
+
+    print(today, type(ten_days), twenty_days, thirty_days)
+
     # print(today, ten_days, twenty_days, thirty_days)
     interval = timedelta(days=1)
 
 
-
-    answers = UserAnswer.objects.all().filter(answer_date__gt=thirty_days).filter(user=user).count()
-    return answers    
+    last10days = UserAnswer.objects.dates('answer_date', 'day').filter(answer_date__gt=ten_days).filter(user=user).count()
+    print(last10days)
+    
+    last11to20days = UserAnswer.objects.dates('answer_date', 'day').filter(answer_date__range=(twenty_days, ten_days)).filter(user=user).count()
+    print(last11to20days)
+    
+    last21to30days = UserAnswer.objects.dates('answer_date', 'day').filter(answer_date__range=(thirty_days, twenty_days)).filter(user=user).count()
+    print(last21to30days)
+    return last10days    
 
 def long_term(user):
     ninety_days = date.today() - timedelta(90)
