06468a25c06896873b895eb89cde9c55eb891cc5
Keith McSpurren <info@track2development.com>
2019-09-09 14:39:37 -0400

    sendgrid variables working...convert to string

--- backend/users/useremails.py
+++ backend/users/useremails.py
@@ -6,23 +6,39 @@ import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
+import json
+
 def userSignUp(emailto):
    send_mail("Thanks for signing up!", "Welcome...", "info@smrtr.life", [emailto])
    # return HttpResponse('%s'%res)
 
 def sendGridmailer(user):
-   message = Mail(
-      from_email='info@smrtr.life',
-      to_emails='info@track2development.com',
-      subject='Makin it rain $$$',
-      html_content='<strong>boom</strong>')
-   try:
-      # sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-      sg = SendGridAPIClient(os.getenv('SENDGRID_API_KEY'))
+   name = str(user)
+   print(name)
+   sg = SendGridAPIClient(os.getenv('SENDGRID_API_KEY'))
 
-      response = sg.send(message)
-      print(response.status_code)
-      print(response.body)
-      print(response.headers)
-   except Exception as e:
-      print(str(e))
+   data = {
+      "personalizations": [
+         {
+            "to": [
+            {
+               "email": "info@track2development.com"
+            }
+            ],
+               "subject": name
+            }
+         ],
+         "from": {
+            "email": "test@example.com"
+         },
+         "content": [
+            {
+               "type": "text/plain",
+               "value": "Hello, Email!"
+            }
+         ]
+   }
+   response = sg.client.mail.send.post(request_body=data)
+   print(response.status_code)
+   print(response.body)
+   print(response.headers)
