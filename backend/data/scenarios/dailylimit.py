4ca4a93962018918bbab79bea106b04139d39cef
Keith McSpurren <info@track2development.com>
2019-09-18 11:14:18 -0400

    fixed delay of questions issue across all users

--- backend/data/scenarios/dailylimit.py
+++ backend/data/scenarios/dailylimit.py
@@ -10,13 +10,13 @@ is then called by the app on the Home page to show the normal carosel of questio
 a message with a countdown clock
 '''
 def dailylimit (user):
-    last_answer = UserAnswer.objects.latest('answer_date')
+    last_answer = UserAnswer.objects.filter(user=user).latest('answer_date')
     last_answer_time = last_answer.answer_date
     now = datetime.now(timezone.utc)
     
     time_gap = now - timedelta(seconds=120)
     # print(time_gap)
-    q_in_last_three_minutes = UserAnswer.objects.filter(answer_date__gt=time_gap).count()
+    q_in_last_three_minutes = UserAnswer.objects.filter(user=user, answer_date__gt=time_gap).count()
     # print(q_in_last_three_minutes)
     if q_in_last_three_minutes >= 2:
         time_unlock = last_answer_time + timedelta(days=0.5) - now
