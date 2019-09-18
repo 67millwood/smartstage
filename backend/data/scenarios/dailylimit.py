b7c11803cb7f18a5420cfbe05bddb19b483c2d68
Keith McSpurren <info@track2development.com>
2019-09-18 13:37:35 -0400

    fix for new users with no questions yet

--- backend/data/scenarios/dailylimit.py
+++ backend/data/scenarios/dailylimit.py
@@ -10,18 +10,21 @@ is then called by the app on the Home page to show the normal carosel of questio
 a message with a countdown clock
 '''
 def dailylimit (user):
-    last_answer = UserAnswer.objects.filter(user=user).latest('answer_date')
-    last_answer_time = last_answer.answer_date
-    now = datetime.now(timezone.utc)
-    
-    time_gap = now - timedelta(seconds=120)
-    # print(time_gap)
-    q_in_last_three_minutes = UserAnswer.objects.filter(user=user, answer_date__gt=time_gap).count()
-    # print(q_in_last_three_minutes)
-    if q_in_last_three_minutes >= 2:
-        time_unlock = last_answer_time + timedelta(days=0.5) - now
-        #time_next = time_unlock.strftime("%A, %I:%M %p")
-        for_api = time_unlock.seconds
-        appdelay = str(time_unlock)
-        # print(appdelay)
-        return {'appdelay': for_api}
+    if UserAnswer.objects.filter(user=user).count() == 0:
+        return {'appdelay': None}
+    else:        
+        last_answer = UserAnswer.objects.filter(user=user).latest('answer_date')
+        last_answer_time = last_answer.answer_date
+        now = datetime.now(timezone.utc)
+        
+        time_gap = now - timedelta(seconds=120)
+        # print(time_gap)
+        q_in_last_three_minutes = UserAnswer.objects.filter(user=user, answer_date__gt=time_gap).count()
+        # print(q_in_last_three_minutes)
+        if q_in_last_three_minutes >= 2:
+                time_unlock = last_answer_time + timedelta(days=0.5) - now
+                #time_next = time_unlock.strftime("%A, %I:%M %p")
+                for_api = time_unlock.seconds
+                appdelay = str(time_unlock)
+                # print(appdelay)
+                return {'appdelay': for_api}
